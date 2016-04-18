3b777b7e0ba7833e5fdac1161c2a7c9587a75592
Dakota S <blackviper96@hotmail.com>
2016-04-18 19:13:07 -0300

    Create example-module.js

---
+++ example-module.js
@@ -0,0 +1,22 @@
+exports.module = {
+	name: "example",
+	requires: ["httphandler"], // This is modules that are a part of the application, like "example" is the name of the module.
+	libraries: ["http","request"], // These are libraries that are installed via npm.
+	failed: false // If the loading failed or not.
+};
+
+exports.module.preinit = function(){ // Do some pre-initialization, like connecting to db.
+	if(global.modules["httphandler"] == undefined){
+		return;
+	}
+};
+
+exports.module.init = function(){
+  // get everything ready to run!
+  
+  setInterval(customFunction, 5); // Example of a task you run with this module.
+};
+
+function customFunction(){
+  // Do some custom things.
+}
