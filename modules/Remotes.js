8490efd8d2ad3e590d6a7459ba1cb4c611e3cc75
Semaphorism <blackviper96@hotmail.com>
2016-06-19 23:32:35 -0300

    Rewrite

--- modules/Remotes.js
+++ modules/Remotes.js
@@ -25,7 +25,7 @@ exports.module.init = function()
 
 }
 
-exports.Server = function(name, port)
+exports.Server = function(name, port, opt)
 {
 	var server = {
 		name: name,
@@ -34,8 +34,12 @@ exports.Server = function(name, port)
 		ClientNum: 1,
 		Events: {},
 		Functions: {},
-		port: port
+		port: port,
+		Auth: false
 	};
+	if(opt !== undefined){
+		if(opt.Auth !== undefined){ server.Auth = opt.Auth; }
+	}
 
 	server.CreateEvent = function(ename, callback){
 		var newEvent = {};
@@ -46,7 +50,9 @@ exports.Server = function(name, port)
 				a: ename,
 				d: data
 			}
-			client.send(JSON.stringify(np));
+			if(client.socket.readyState == WebSocket.OPEN){
+				client.socket.send(JSON.stringify(np));
+			}
 		}
 
 		newEvent.FireAllClients = function(data, flags)
@@ -61,8 +67,43 @@ exports.Server = function(name, port)
 
 	server.OnServerReceive = function(client, data, flags)
 	{
-		console.log("Receive: "+data);
+		try {
+			var j = JSON.parse(data);
+			if(j.headers !== undefined){
+				client.headers = j.headers;
+				if(client.headers.Auth !== server.Auth && server.Auth !== false){
+					client.socket.terminate();
+					delete client;
+					return;
+				} else {
+
+				}
+			}
+			if(client.headers === false){
+				return;
+			}
+			if(server.Events[j.a] !== undefined && server.Events[j.a].Callback !== undefined){
+				server.Events[j.a].Callback(client, j.d, flags);
+			}
+		} catch(err){
+			if(client.headers === false){
+				client.socket.terminate();
+				delete client;
+			}
+		}
 	}
+
+	server.ClientTimer = setInterval(function(){
+		for(var i in server.Clients){
+			if(server.Auth !== false && (server.Clients[i].headers === false || server.Clients[i].headers.Auth !== server.Auth)){
+				if(server.Clients[i].start + 2 <= time_now()){
+					console.log("Closing client "+server.Clients[i].socket.address)
+					server.Clients[i].socket.terminate();
+					delete server.Clients[i];
+				}
+			}
+		}
+	}, 100);
 
 	server.socket = CreateServerSocket(server);
 	return server;
@@ -73,18 +114,23 @@ function CreateServerSocket(server)
 	server.socket = new WebSocketServer({ port: server.port });
 	server.socket.on('connection', function connection(clientSock) {
 		server.ClientNum++;
+		console.log("New connection from "+clientSock.upgradeReq.connection.remoteAddress);
 		clientSock.on('message', function(msg, flags){
-			server.OnServerReceive(clientSock, msg, flags);
+			server.OnServerReceive(server.Clients[server.ClientNum], msg, flags);
 		});
-		//clientSock.on('ping', server.ping);
 		clientSock.on('close', function(msg){
+			try {
+				//server.Clients[server.ClientNum].socket.terminate();
+			} catch(err){
 
+			}
+			delete server.Clients[server.ClientNum];
 		});
 
-		server.Clients[server.ClientNum] = clientSock;
+		server.Clients[server.ClientNum] = {socket: clientSock, headers: false, start: time_now()}
 	});
 
-	server.socket.on('error', function(){
+	server.socket.on('error', function(err){
 		console.log(err.stack);
 	});
 }
@@ -114,7 +160,7 @@ exports.Client = function(name, location, flags)
 			} else {
 				var j = JSON.parse(data)
 				if(client.Events[j.a] != undefined){
-					client.Events[j.a](j.d);
+					client.Events[j.a].Callback(j.d, flags);
 				}
 			}
 		} catch(err){
@@ -124,8 +170,34 @@ exports.Client = function(name, location, flags)
 		}
 	}
 
-	client.socket = CreateSocket(client);
+	client.CreateEvent = function(name, callback)
+	{
+		var event = {
+			Name: name,
+			Callback: callback,
+			Attempts: 0
+		};
 
+		event.Fire = function(data, flags)
+		{
+			var np = {
+				a: event.Name,
+				d: data
+			}
+			if(client.socket.readyState == WebSocket.OPEN){
+				client.socket.send(JSON.stringify(np));
+			} else {
+				event.Attempts++;
+				if(event.Attempts >= 10){
+					console.log("Web socket is not in its OPEN state");
+					event.Attempts = 0;
+				}
+			}
+		}
+		client.Events[name] = event;
+	}
+
+	client.socket = CreateSocket(client);
 	return client;
 }
 
@@ -134,16 +206,19 @@ exports.reconnect = function(obj)
 	obj.socket = CreateSocket(obj);
 }
 
-function CreateSocket(obj)
+function CreateSocket(obj, objheaders)
 {
+	var packet = {headers: false};
+	if(objheaders !== undefined){
+		packet.headers = objheaders;
+	}
 	if(obj.l == undefined){
 		console.log("Can't open socket, location missing.");
 		return;
 	}
 	var socket = new WebSocket(obj.l);
 	socket.on('open', function open() {
-		console.log(obj.name +" socket opened.");
-		socket.ping(obj.name);
+		socket.send(JSON.stringify(packet));
 	});
 
 	socket.on('message', function(data, flags) {
@@ -153,9 +228,9 @@ function CreateSocket(obj)
 	socket.on("error", function(err){
 		if(socket.readyState == WebSocket.CLOSED || socket.readyState == WebSocket.CLOSING){
 			setTimeout(function(){
-				exports.reconnect(connections[obj.name])
+				exports.reconnect(obj)
 			}, 5000);
-			console.log("Socket has errored, reconnecting.");
+			console.log("Socket has errored, reconnecting. "+err.stack);
 		} else {
 			console.log(err.stack);
 		}
